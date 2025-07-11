93ae222fb5345c8501d543ee3a1acf7268c0ba39
wxd-gaming <492794628@qq.com>
2025-07-11 17:32:10 +0800

    【新增】拉取当前房间用户列表

---
+++ wxdgaming.webim.service/src/main/java/wxdgaming/webim/service/module/chat/processor/RoomUserListProcessor.java
@@ -0,0 +1,47 @@
+package wxdgaming.webim.service.module.chat.processor;
+
+import com.alibaba.fastjson.JSONObject;
+import com.google.inject.Singleton;
+import lombok.extern.slf4j.Slf4j;
+import wxdgaming.boot2.core.lang.RunResult;
+import wxdgaming.boot2.starter.net.SocketSession;
+import wxdgaming.webim.service.bean.ChatRoom;
+import wxdgaming.webim.service.bean.ChatUser;
+import wxdgaming.webim.service.module.chat.AbstractProcessor;
+
+import java.util.ArrayList;
+
+/**
+ * 获取当前房间用户
+ *
+ * @author: wxd-gaming(無心道, 15388152619)
+ * @version: 2025-07-10 11:29
+ **/
+@Slf4j
+@Singleton
+public class RoomUserListProcessor extends AbstractProcessor {
+
+    @Override public String type() {
+        return "roomUserList";
+    }
+
+    @Override public void process(SocketSession socketSession, ChatUser self, JSONObject jsonObject) {
+        long roomId = jsonObject.getLongValue("roomId");
+        ChatRoom chatRoom = dataService.getRoomMap().get(roomId);
+        if (chatRoom == null) {
+            chatService.fail(socketSession, "房间不存在");
+            return;
+        }
+        boolean hasUser = chatRoom.hasUser(self.getName());
+        if (!hasUser) {
+            chatService.fail(socketSession, "尚未加入该房间");
+            return;
+        }
+        RunResult ok = RunResult.ok();
+        ok.fluentPut("cmd", "roomUserList");
+        ok.fluentPut("roomId", roomId);
+        ok.fluentPut("userList", new ArrayList<>(chatRoom.getUserMap()));
+        socketSession.write(ok.toJSONString());
+    }
+
+}
